202b6ba70b34b84631a481ef8ab243654cfd8223
PrinceNandha <nandhaprince1601@gmail.com>
2021-04-11 10:12:16 +0530

    shipping

---
+++ user/cart/checkout/shippingInfo.php
@@ -0,0 +1,71 @@
+<?php 
+
+    include '../../../config.php';
+
+    session_start();
+
+    error_reporting(0);
+
+    if(!isset($_SESSION['uid'])){
+        header("Location : ../loginsystemUsers/index.php");
+    }
+
+    $user = $_SESSION['uid'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="shipping.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <ul class="cards">
+        
+      <?php
+      
+          $sql = "SELECT * FROM shippinginfo WHERE userid=$user";
+          $result = mysqli_query($conn,$sql);
+          $address = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+          foreach($address as $ad):
+            $adid = $ad['adressid'];
+      ?>
+        <li class="cards__item">
+          <div class="card">
+            <div class="card__content">
+              <!-- <div class="card__title">Flex</div> -->
+              <p class="card__text">
+                <?php echo $ad['name'];?>
+                <?php echo $ad['contact'];?>
+                <?php echo $ad['address'];?>
+                <?php echo $ad['pincode'];?>
+              </p>
+              <button class="btn btn--block card__btn"><a href='<?php echo "updateshipping.php?adid=$adid";?>'>Update</a></button>
+              <button class="btn btn--block card__btn"><a href='<?php echo "updateshipping.php?adid=$adid";?>'>Remove</a></button>
+            </div>
+          </div>
+        </li>
+      
+      
+        <?php endforeach;?>
+        
+        <li class="cards__item">
+          <div class="card">
+            <!-- <div class="card__image card__image--flowers"></div> -->
+            <div class="card__content">
+              <div class="card__title">Flex Basis</div>
+              <p class="card__text"></p>
+              <button class="btn btn--block card__btn"><a href='<?php echo "shippingInfoadd.php";?>'>Add new address</a></button>
+            </div>
+          </div>
+        </li>
+
+    </ul>
+    
+    
+</body>
+</html>
--- user/cart/checkout/shippingInfoadd.php
+++ user/cart/checkout/shippingInfoadd.php
@@ -19,20 +19,27 @@ include '../../../config.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="checkout.css">
+    <link rel="stylesheet" type="text/css" href="shippingInfoadd.css">
 
     <title>Document</title>
 </head>
 <body>
 	<div class="container">
-		<form action="" method="POST" class="login-email">
+		<form action="checkout.inc.php" method="POST" class="login-email">
 			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
 			<div class="input-group">
-				<input type="fullname" placeholder="Full Name" name="fullname" value="<?php echo $email; ?>" required>
+				<input type="text" placeholder="Full Name" name="fullname"required>
 			</div>
 			<div class="input-group">
-				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+				<input type="text" placeholder="Address" name="adress" required>
 			</div>
+			<div class="input-group">
+				<input type="number" placeholder="Contact" name="contact" required>
+			</div>
+			<div class="input-group">
+				<input type="number" placeholder="Pin Code" name="pincode" min="100000" max="99999" required>
+			</div>
+			
 			<div class="input-group">
 				<button name="submit" class="btn">Login</button>
 			</div>
